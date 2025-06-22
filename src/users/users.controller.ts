d90ce295c42b19b959692dabdfbb59fdb74c63ae
CarolineT9 <caroltvrs@gmail.com>
2025-06-22 20:09:43 -0300

    feat: adiciona rota para atualizar usuário

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersService } from './users.service';
-import { Controller, Get, Param, ParseIntPipe, Post, Body } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Post, Body, Patch } from '@nestjs/common';
 //Buscar detalhes de um usuario
 //Cadastrar usuário
 //Deletar usuário
@@ -15,5 +16,10 @@ export class UsersController {
   createUser(@Body() createUserDto: CreateUserDto){
     return this.UsersService.create(createUserDto)
   }
+  @Patch(':id')
+  updateUser(@Param('id', ParseIntPipe) id:number, @Body() updateUserDto: UpdateUserDto){
+    return this.UsersService.update(id, updateUserDto)
+  }
+  
 
 }
