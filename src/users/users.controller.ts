0f576b2004d551c5c87aefa9bb2f666a33dff477
CarolineT9 <caroltvrs@gmail.com>
2025-06-22 19:17:13 -0300

    feat: cria camada de usuarios - cria user controller, usermodule e user service, cadastra tabela schema.prima e cria migrate

---
+++ src/users/users.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+
+  @Get(':id')
+
+  findOneUser(){
+    return 'Rota funcionou'
+  }
+
+}
